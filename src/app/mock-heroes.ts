1b2b4b6605462ecae766ffb9cd3e99d3b9cd0bb4
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2019-07-02 21:31:07 -0300

    add conditional *ngIf

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Homem de Ferro' },
+  { id: 12, name: 'Capitão América' },
+  { id: 13, name: 'Thor' },
+  { id: 14, name: 'Hulk' },
+  { id: 15, name: 'Viúva Negra' },
+  { id: 16, name: 'Gavião Arqueiro' },
+  { id: 17, name: 'Homem Formiga' },
+  { id: 18, name: 'Visão' },
+  { id: 19, name: 'Feiticeira Escarlate' },
+  { id: 20, name: 'Capitã Marvel' }
+];
